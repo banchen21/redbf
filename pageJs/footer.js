aa68e09d82a00a75e6d843a82ea40cd4356fcdb5
banchen21 <banchen19@outlook.com>
2024-12-23 04:03:24 +0800

    1

---
+++ pageJs/footer.js
@@ -0,0 +1,28 @@
+document.writeln(`
+<!-- Footer -->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="text-container about">
+                    <h4>关于更多</h4>
+                        <p style="color:#dfe5ec;margin-bottom:0rem">技术By <a href="https://github.com/banchen19/">半尘</a>
+                        </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Copyright -->
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p style="color:#dfe5ec;margin-bottom:0rem"> 友情外链&nbsp;<a target="_blank"
+                        href="https://minecraft.net/">Minecraft</a>&nbsp;&nbsp;</p>
+                <p class="p-small">MIT License Copyright &copy; 2024 <a target="_blank"
+                        href="https://github.com/banchen19/">banchen19丨版权所有</a></p>
+            </div> <!-- end of col -->
+        </div> <!-- enf of row -->
+    </div> <!-- end of container -->
+</div> <!-- end of copyright -->
+`)
---
+++ pageJs/groupChat.js
@@ -0,0 +1,3 @@
+document.writeln(`
+<a class="btn-solid-reg page-scroll" href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=tXZZEb2v7LrNhllJGzDGM79wcBtHyeu3&authKey=201Io4BQYU%2B24h72bXS3RlR46z1mYpILVQwoIsS48bFnaTRXXbOzplgXqpoCLV6a&noverify=0&group_code=1012725613" target="_blank">点击加群</a></h3>
+`)
